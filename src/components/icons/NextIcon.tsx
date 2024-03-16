e858a45abde9e3e09aa0de57f942093c8382af38
JuanDNJ <juandevnjv@gmail.com>
2024-03-16 10:06:36 +0100

    Fin Maquetacion

--- src/components/icons/NextIcon.tsx
+++ src/components/icons/NextIcon.tsx
@@ -1,11 +1,13 @@
 export const NextIcon = ({ color = "#333" }) => {
- return (<svg
-  xmlns="http://www.w3.org/2000/svg"
-  height="24"
-  viewBox="0 -960 960 960"
-  width="24"
-  fill={color}
->
-  <path d="M647-440H160v-80h487L423-744l57-56 320 320-320 320-57-56 224-224Z" />
-</svg>)
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height="24"
+      viewBox="0 -960 960 960"
+      width="24"
+      fill={color}
+    >
+      <path d="M647-440H160v-80h487L423-744l57-56 320 320-320 320-57-56 224-224Z" />
+    </svg>
+  );
 };
