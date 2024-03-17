36dfe7e43782e164cd3eceded97b9515acf73b22
JuanDNJ <juandevnjv@gmail.com>
2024-03-17 07:25:40 +0100

    "modified:   vite.config.ts"

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/S5-onboarding-digital/',
+  publicDir: "public",
   plugins: [react()],
 })
